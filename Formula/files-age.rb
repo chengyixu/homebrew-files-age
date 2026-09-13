4bc16678e4dc3a3167e5643166af0850cfb84ac0
Chengyi Xu <noreply@users.noreply.github.com>
2026-09-13 15:24:34 +0800

    Add files-age Homebrew formula

---
+++ Formula/files-age.rb
@@ -0,0 +1,17 @@
+class FilesAge < Formula
+  desc "List file modification ages from the command line"
+  homepage "https://github.com/chengyixu/files-age"
+  url "https://github.com/chengyixu/files-age/releases/download/v1.0.3/chengyixu-files-age-1.0.3.tgz"
+  sha256 "ac9711dfcdcd869e50715de62e9305ef46dd189f086229b15f5e3209646a21f7"
+  license "MIT"
+
+  depends_on "node"
+
+  def install
+    bin.install "package/index.js" => "files-age"
+  end
+
+  test do
+    system "#{bin}/files-age", "--help"
+  end
+end
---
+++ README.md
@@ -0,0 +1,9 @@
+# Homebrew tap for files-age
+
+Install the public files-age release with:
+
+```sh
+brew install chengyixu/files-age/files-age
+```
+
+The formula downloads the immutable v1.0.3 GitHub Release asset and verifies its SHA-256 checksum before installation.
